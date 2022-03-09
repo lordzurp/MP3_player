06d7f2fde3c90266eadbaa454807b7683fbda4ab
Ti nicO - U-zur-P <zurp.online@gmail.com>
2022-03-09 14:05:15 +0100

    v2

--- mp3_player_pro/mp3_player_pro.ino
+++ mp3_player_pro/mp3_player_pro.ino
@@ -14,42 +14,24 @@
 #include <DFRobot_DF1201S.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial DF1201SSerial(0, 2);  //RX  TX
+int sensorPin = A0;    // select the input pin for the potentiometer
+SoftwareSerial DF1201SSerial(D3, D4);  //RX  TX
 
 DFRobot_DF1201S DF1201S;
 void setup(void){
-  pinMode(LED_BUILTIN, OUTPUT);
+/*  pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(2000);                       // wait for a second
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(100);                       // wait for a second*/
   DF1201SSerial.begin(115200);
   while(!DF1201S.begin(DF1201SSerial)){
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
     delay(1000);
   }
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  /*Set volume to 20*/
   DF1201S.setVol(/*VOL = */20);
-  /*Enter music mode*/
   DF1201S.switchFunction(DF1201S.MUSIC);
-  /*Wait for the end of the prompt tone */
-  delay(2000);
-  /*Set playback mode to "repeat all"*/
   DF1201S.setPlayMode(DF1201S.ALLCYCLE);
 
-  //Turn on indicator LED (Power-down save)
-  DF1201S.setLED(true);
   //Turn on the prompt tone (Power-down save) 
   DF1201S.setPrompt(false);
   //Enable amplifier chip 
@@ -60,12 +42,6 @@ void setup(void){
 }
 
 void loop(){
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  DF1201S.setVol(map(analogRead(sensorPin), 0 , 1023, 0, 30));
+  delay(100);
 }
